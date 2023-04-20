7718f083be81f9065b54365ca8acf15a6ab0cc75
noearobles <roblesnoe1030@gmail.com>
2023-04-20 17:42:40 -0500

    modifed js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import './App.css';
 import BeerCard from './BeerCard';
 
 class App extends Component {
-
   constructor() {
     super()
 
     this.state = {
-      arrayOBeer: [],
+      arrayOfBeer: [],
       likedBeer: []
     }
   }
 
   componentDidMount() {
     axios.get('https://api.punkapi.com/v2/beers?page=2&per_page=80').then(res => {
-      const arrayOBeer = res.data
-      this.setState({ arrayOBeer: arrayOBeer })
+      const arrayOfBeer = res.data
+      this.setState({ arrayOfBeer: arrayOfBeer })
 
     })
   }
@@ -28,7 +27,7 @@ class App extends Component {
     return (
       <div className="App" >
         <header className="App-header">
-          <ol>{this.state.arrayOBeer.map((beer, index) => {
+          <ol>{this.state.arrayOfBeer.map((beer, index) => {
             return (
               <BeerCard key={index} name={beer.name} image_url={beer.image_url} first_brewed={beer.first_brewed} tagline={beer.tagline} abv={beer.abv} description={beer.description} />
             )
--- src/BeerCard.js
+++ src/BeerCard.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 
 function BeerCard(props) {
+    const cardStyle = {
+        display: 'flex',
+        flexDirection: 'row'
+    }
     const [like, setLike] = useState(false)
     const { name, image_url, first_brewed, tagline, abv, description } = props;
     return (
-        <li style={{ listStyle: 'none' }}>
+        <div style={cardStyle}>
             <img style={{ height: '200px' }} src={image_url}></img>
-            <h3>{name}<span>{first_brewed}</span></h3>
-            <button onClick={() => setLike((prevLike) => !prevLike)}>Like:{like ? '❤️' : '🤍'}</button>
-            <h4>{tagline}</h4>
-            <p><span>{abv}</span>{description}</p>
-        </li>
+            <li style={{ listStyle: 'none' }}>
+                <h3>{name}<span> {first_brewed}</span></h3>
+                <h4>{tagline}</h4>
+                <button onClick={() => setLike((prevLike) => !prevLike)}>Like:{like ? '❤️' : '🤍'}</button>
+                <p><span>{abv}</span>{description}</p>
+            </li></div>
     )
 }
 
