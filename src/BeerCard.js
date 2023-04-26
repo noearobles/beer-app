28aa0944f1815561216675b014005f9375f9fe31
noaarobles <roblesnoe1030@gmail.com>
2023-04-25 23:52:58 -0500

    modified js

--- src/BeerCard.js
+++ src/BeerCard.js
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
 
 function BeerCard(props) {
-    const [like, setLike] = useState(false)
-    const { name, image_url, first_brewed, tagline, abv, description } = props;
-    return (
-        <div className="Beer-Card">
-            <li style={{ listStyle: 'none' }}>
-                <img style={{ height: '200px' }} src={image_url} alt="beerImage"></img>
-                <h3>{name}<span> {first_brewed}</span></h3>
-                <h4>{tagline}</h4>
-                <div className="stats"><p><span>{abv} </span>{description}</p>
-                    <button onClick={() => setLike((prevLike) => !prevLike)}>Like:{like ? '❤️' : '🤍'}</button></div>
-            </li></div>
-    )
+  const [like, setLike] = useState(false);
+  const { name, image_url, first_brewed, tagline, abv, description } = props;
+  return (
+    <div className="Beer-Card">
+      <li style={{ listStyle: "none" }}>
+        <img style={{ height: "200px" }} src={image_url} alt="beerImage"></img>
+        <h3>
+          {name}
+          <span> {first_brewed}</span>
+        </h3>
+        <h4>{tagline}</h4>
+        <div className="stats">
+          <ul style={{ listStyle: "none" }}>
+            <li>
+              <b>ABV: {abv}</b>
+            </li>
+            <b>Description: </b>
+            {description}
+          </ul>
+          <button onClick={() => setLike((prevLike) => !prevLike)}>
+            Like:{like ? "❤️" : "🤍"}
+          </button>
+        </div>
+      </li>
+    </div>
+  );
 }
 export default BeerCard;
